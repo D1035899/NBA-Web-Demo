2f7ef884ce36c5fd0626194f886caacd2c3b4c87
D1035899 <D1035899@o365.fcu.edu.tw>
2024-11-06 04:10:17 +0800

    feat: Update player selection functionality, add API to retrieve player list based on team.

--- app.py
+++ app.py
@@ -2,38 +2,52 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import pandas as pd
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, jsonify, request
 import sqlite3
 from modules.predict import PredictModel
 
 app = Flask(__name__)
 
-def get_players():
+# 依據選擇的隊伍取得球員名單
+def get_players_by_team(team_abbreviation):
     conn = sqlite3.connect('./data/NBA.db')
     cursor = conn.cursor()
 
-    cursor.execute('SELECT playerid, name FROM players')
-    playersBasicData = cursor.fetchall()
+    cursor.execute('SELECT playerid, name, team FROM players WHERE team = ?', (team_abbreviation,))
+    players = cursor.fetchall()
 
     conn.close()
-    return playersBasicData
+    return [{'id': player[0], 'name': player[1], 'team': player[2]} for player in players]
 
+#取得球隊名單
 def get_teams():
     conn = sqlite3.connect('./data/NBA.db')
     cursor = conn.cursor()
 
-    cursor.execute('SELECT teamid, name FROM teams')
-    teamsBasicData = cursor.fetchall()
+    cursor.execute('SELECT teamid, name, abbreviation FROM teams')
+    teamRoster = cursor.fetchall()
 
     conn.close()
-    return teamsBasicData
+    return teamRoster
 
+'''
+    api part
+'''
+
+# 取得首頁
 @app.route('/')
 def index():
-    players = get_players()
-    teams = get_teams()
-    return render_template('index.html', players=players, teams=teams)
+    teamRoster = get_teams()
+    teamRoster = [(team[1], team[2]) for team in teamRoster] # team[0]: name, team[1]: abbreviation
+    return render_template('index.html', teams=teamRoster)
 
+# 取得球員名單，並回傳json形式的名單
+@app.route('/get_players/<team_abbreviation>')
+def get_players(team_abbreviation):
+    players = get_players_by_team(team_abbreviation)
+    return jsonify({'players': players})
+
+#進行預測
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
@@ -41,16 +55,25 @@ def predict():
         data = pd.read_csv("./data/mixData.csv")
         file_path = './data/selected_columns_s.txt'
         prediction = PredictModel(model, data, file_path)
+        conn = sqlite3.connect('./data/NBA.db')
+        cursor = conn.cursor()
         selectedTeam1 = request.form.get('selectTeam1')
+        cursor.execute('SELECT teamid FROM teams WHERE abbreviation = ?', (selectedTeam1,))
+        selectedTeam1 = cursor.fetchall()
+        print(selectedTeam1)
         selectedTeam2 = request.form.get('selectTeam2')
+        cursor.execute('SELECT teamid FROM teams WHERE abbreviation = ?', (selectedTeam2,))
+        selectedTeam2 = cursor.fetchall()
+
+        selectedTeam1 = selectedTeam1[0][0]
+        selectedTeam2 = selectedTeam2[0][0]
         selectedPlayers1 = list(map(int, request.form.getlist('selectedPlayer1')))
         selectedPlayers2 = list(map(int, request.form.getlist('selectedPlayer2')))
 
-
-        print(f"team1: {selectedTeam1}")
-        print(f"player: {selectedPlayers1}\n")
-        print(f"team2: {selectedTeam2}")
-        print(f"player: {selectedPlayers2}")
+        # print(selectedTeam1)
+        # print(selectedTeam2)
+        # print(selectedPlayers1)
+        # print(selectedPlayers2)
 
         result, probabilities = prediction.predict_game(selectedPlayers1, selectedPlayers2, selectedTeam1, selectedTeam2, selectedTeam1)
         prediction.print_prediction_result(result, probabilities)
@@ -61,8 +84,11 @@ def predict():
         else :
             win = 2
             winResult = "客場獲勝"
+        selectedTeam1 = str(selectedTeam1)
+        selectedTeam2 = str(selectedTeam2)
         return render_template('predict.html', winResult=winResult, players1=selectedPlayers1, players2=selectedPlayers2, team1=selectedTeam1, team2=selectedTeam2)
 
+# 返回首頁
 @app.route('/back', methods=['GET'])
 def back():
     return redirect(url_for('index'))
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-
+    // 初始化隊伍以及球員選單
     $(".team-select").select2({
         placeholder: "請選擇球隊",
-        allowClear: true,
+        // allowClear: true,
         width: "100%",
         language: {
             noResults: function () {
@@ -14,7 +14,7 @@ $(document).ready(function () {
         // matcher: matchCustom,
     });
     $(".player-select").select2({
-        placeholder: "請選擇5位球員",
+        placeholder: "請先選擇球隊",
         allowClear: true,
         width: "100%",
         language: {
@@ -29,7 +29,7 @@ $(document).ready(function () {
         matcher: matchCustom,
     });
 
-    // 如果要在選項顯示id
+    // 如果要在選項顯示id, 請將該函示取消註解，並且將含有該函示名稱的地方也取消註解
     // function formatPlayer(player) {
     //     if (!player.id) {
     //         return player.text;
@@ -46,11 +46,47 @@ $(document).ready(function () {
     //     return playerName ? playerName : player.text;
     // }
 
+    //更新球員名單
+    function updatePlayers(teamSelectId, playersDivId) {
+    const teamSelectElement = document.getElementById(teamSelectId).value;
+    fetch(`/get_players/${teamSelectElement}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const playersDiv = document.getElementById(playersDivId);
+            playersDiv.innerHTML = ""; // 清除現有的球員名單
+            // 建立球員名單
+            data.players.forEach((player) => {
+                const option = document.createElement("option");
+                option.setAttribute("data-name", player.name);
+                option.value = player.id;
+                option.innerHTML = player.name;
+                playersDiv.appendChild(option);
+            });
+
+            // 更新 Select2 套件
+            $(`#${playersDivId}`).select2({
+                placeholder: "請選擇5位球員",
+                // allowClear: true,
+                width: "100%",
+                language: {
+                    noResults: function () {
+                        return "找不到符合的球員";
+                    },
+                },
+                closeOnSelect: true,
+                maximumSelectionLength: 5,
+            });
+        })
+        .catch((error) => {
+            console.error('Error fetching players:', error);
+        });
+}
+
+    //建立球員搜尋欄的搜尋規則
     function matchCustom(params, data) {
         if ($.trim(params.term) === "") {
             return data;
         }
-
         if (
             data.id.toString().indexOf(params.term) > -1 ||
             $(data.element)
@@ -60,23 +96,24 @@ $(document).ready(function () {
         ) {
             return data;
         }
-
         return null;
     }
 
+    // 若其中一個球員被選走，則隔壁選單的該球員要被disabled
     function disableOptionInOtherSelect(selectedValues, otherSelect) {
         $(otherSelect)
-            .find("option")
-            .each(function () {
-                if (selectedValues.includes($(this).val())) {
-                    $(this).prop("disabled", true);
-                } else {
-                    $(this).prop("disabled", false);
-                }
-            });
+        .find("option")
+        .each(function () {
+            if (selectedValues.includes($(this).val())) {
+                $(this).prop("disabled", true);
+            } else {
+                $(this).prop("disabled", false);
+            }
+        });
         $(otherSelect).trigger("change.select2");
     }
 
+    // 若其中一個隊伍被選走，則隔壁選單的該隊伍要被disabled
     function disableOptionInAnotherTeam(selectedValue, targetSelect) {
         $(targetSelect)
             .find("option")
@@ -89,25 +126,37 @@ $(document).ready(function () {
             });
         $(targetSelect).trigger("change.select2");
     }
+
+    // 偵測球員是否被選取
     $("#team1Players").on("change", function () {
         var selectedValues = $(this).val() || [];
         disableOptionInOtherSelect(selectedValues, "#team2Players");
     });
 
+    // 偵測球員是否被選取
     $("#team2Players").on("change", function () {
         var selectedValues = $(this).val() || [];
         disableOptionInOtherSelect(selectedValues, "#team1Players");
     });
+
+    // 偵測主場隊伍是否被選取，並且根據選取的隊伍新增球員名單
     $("#team1").on("change", function () {
+        const playerSelect = document.getElementById("team1Players");
+        playerSelect.removeAttribute("disabled");
         var selectedValues = $(this).val() || [];
         disableOptionInAnotherTeam(selectedValues, "#team2");
+        updatePlayers("team1", "team1Players");
     });
+
+    // 偵測客場隊伍是否被選取，並且根據選取的隊伍新增球員名單
     $("#team2").on("change", function () {
+        const playerSelect = document.getElementById("team2Players");
+        playerSelect.removeAttribute("disabled");
         var selectedValues = $(this).val() || [];
         disableOptionInAnotherTeam(selectedValues, "#team1");
+        updatePlayers("team2", "team2Players");
     });
 });
-
 
 // 判斷表單有沒有填好
 $("form").on("submit", function (e) {
@@ -124,5 +173,4 @@ $("form").on("submit", function (e) {
         e.preventDefault();
         alert("每個隊伍必須選擇5位球員");
     }
-
 });
